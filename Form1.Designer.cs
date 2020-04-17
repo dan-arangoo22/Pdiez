59e17d21a99d182ff6e302a69cd58a52c2fa5b3e
dan-arangoo22 <61323944+dan-arangoo22@users.noreply.github.com>
2020-04-17 17:51:54 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Pdiez
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.Bborrar = new System.Windows.Forms.Button();
+			this.multiploUni = new System.Windows.Forms.Button();
+			this.multiploSerie = new System.Windows.Forms.Button();
+			this.Fibonacci = new System.Windows.Forms.Button();
+			this.Factoriales = new System.Windows.Forms.Button();
+			this.lista = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(183, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(415, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(51, 108);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(110, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(168, 108);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(97, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(271, 108);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(90, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(51, 138);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(110, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(167, 138);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(98, 23);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(271, 138);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(90, 23);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(51, 167);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(110, 23);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(168, 167);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(97, 23);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(271, 167);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(90, 23);
+			this.button9.TabIndex = 9;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(108, 196);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(214, 23);
+			this.button10.TabIndex = 10;
+			this.button10.Text = "0";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// Bborrar
+			// 
+			this.Bborrar.Location = new System.Drawing.Point(367, 108);
+			this.Bborrar.Name = "Bborrar";
+			this.Bborrar.Size = new System.Drawing.Size(87, 23);
+			this.Bborrar.TabIndex = 12;
+			this.Bborrar.Text = "Borrar";
+			this.Bborrar.UseVisualStyleBackColor = true;
+			this.Bborrar.Click += new System.EventHandler(this.Bborrar_Click);
+			// 
+			// multiploUni
+			// 
+			this.multiploUni.Location = new System.Drawing.Point(51, 288);
+			this.multiploUni.Name = "multiploUni";
+			this.multiploUni.Size = new System.Drawing.Size(148, 23);
+			this.multiploUni.TabIndex = 17;
+			this.multiploUni.Text = "multiploU";
+			this.multiploUni.UseVisualStyleBackColor = true;
+			this.multiploUni.Click += new System.EventHandler(this.button11_Click_1);
+			// 
+			// multiploSerie
+			// 
+			this.multiploSerie.Location = new System.Drawing.Point(206, 287);
+			this.multiploSerie.Name = "multiploSerie";
+			this.multiploSerie.Size = new System.Drawing.Size(155, 23);
+			this.multiploSerie.TabIndex = 18;
+			this.multiploSerie.Text = "multiploS";
+			this.multiploSerie.UseVisualStyleBackColor = true;
+			this.multiploSerie.Click += new System.EventHandler(this.multiploSerie_Click);
+			// 
+			// Fibonacci
+			// 
+			this.Fibonacci.Location = new System.Drawing.Point(51, 318);
+			this.Fibonacci.Name = "Fibonacci";
+			this.Fibonacci.Size = new System.Drawing.Size(148, 23);
+			this.Fibonacci.TabIndex = 19;
+			this.Fibonacci.Text = "fivonacci";
+			this.Fibonacci.UseVisualStyleBackColor = true;
+			this.Fibonacci.Click += new System.EventHandler(this.Fibonacci_Click);
+			// 
+			// Factoriales
+			// 
+			this.Factoriales.Location = new System.Drawing.Point(205, 316);
+			this.Factoriales.Name = "Factoriales";
+			this.Factoriales.Size = new System.Drawing.Size(156, 23);
+			this.Factoriales.TabIndex = 21;
+			this.Factoriales.Text = "factoriales";
+			this.Factoriales.UseVisualStyleBackColor = true;
+			this.Factoriales.Click += new System.EventHandler(this.Factoriales_Click);
+			// 
+			// lista
+			// 
+			this.lista.FormattingEnabled = true;
+			this.lista.ItemHeight = 16;
+			this.lista.Location = new System.Drawing.Point(526, 117);
+			this.lista.Name = "lista";
+			this.lista.Size = new System.Drawing.Size(152, 180);
+			this.lista.TabIndex = 22;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lista);
+			this.Controls.Add(this.Factoriales);
+			this.Controls.Add(this.Fibonacci);
+			this.Controls.Add(this.multiploSerie);
+			this.Controls.Add(this.multiploUni);
+			this.Controls.Add(this.Bborrar);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button Bborrar;
+		private System.Windows.Forms.Button multiploUni;
+		private System.Windows.Forms.Button multiploSerie;
+		private System.Windows.Forms.Button Fibonacci;
+		private System.Windows.Forms.Button Factoriales;
+		private System.Windows.Forms.ListBox lista;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pdiez
+{
+	public partial class Form1 : Form
+	{
+
+		private int a;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "1";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "2";
+		}
+
+		private void button11_Click(object sender, EventArgs e) // boton =
+		{
+			textBox1.Text = textBox1.Text + "=";
+		}
+
+		private void button10_Click(object sender, EventArgs e) //boton 0
+		{
+			textBox1.Text = textBox1.Text + "0";
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "3";
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "4";
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "5";
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "6";
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "7";
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "8";
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = textBox1.Text + "9";
+		}
+
+		private void button11_Click_1(object sender, EventArgs e)
+		{
+			lista.Items.Clear();
+			a = int.Parse(textBox1.Text);
+
+			int n;
+
+			for (n = 0; n <= 10; n++)
+			{
+
+				int b = a*n;
+
+				lista.Items.Add(b.ToString());
+
+			}
+
+		}
+
+		private void Bborrar_Click(object sender, EventArgs e)
+		{
+
+			textBox1.Clear();
+			lista.Items.Clear();
+
+		}
+
+		private void multiploSerie_Click(object sender, EventArgs e)
+		{
+			lista.Items.Clear();
+			a = int.Parse(textBox1.Text);
+			int n;
+			for ( n =0; n<= a; n++) {
+				double b = Math.Pow(n,2);
+
+				lista.Items.Add(b.ToString());
+}
+		}
+
+		private void Fibonacci_Click(object sender, EventArgs e)
+		{
+			a = int.Parse(textBox1.Text);
+
+			lista.Items.Clear();
+
+			int x = 0;
+			int y = 1;
+			int z = 1;
+
+			for (int b = 0; b <= a;b++)
+			{
+				lista.Items.Add(x.ToString());
+
+				x = y;
+				y = z;
+				z = x + y;
+			}
+
+		}
+
+		private void Factoriales_Click(object sender, EventArgs e)
+		{
+			a = int.Parse(textBox1.Text);
+
+			int c = 1;
+			lista.Items.Clear();
+			for (int b = a; b >= 1; b--)
+			{
+
+				c = c * b;
+
+				textBox1.Text = c.ToString();
+
+			}
+
+
+		}
+	}
+}
